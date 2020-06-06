4843e3681d90eabe595ead3d5030db82796195fd
AllusInteractive <AllusInteractive@users.noreply.github.com>
2020-06-06 21:59:12 +0100

    Started work on iplementing collection functionality

---
+++ TimePortals/Assets/Scripts/CollectableContainer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableContainer : MonoBehaviour
+{
+    // private AudioSource collectableSFX;
+
+    void Awake()
+    {
+        // collectableSFX = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                if (raycastHit.collider.CompareTag("Collectable"))
+                {
+                    gameObject.SetActive(false);
+                    // collectableSFX.Play();
+                }
+            }
+        }
+    }
+}
